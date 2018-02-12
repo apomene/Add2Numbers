988b6b3132968e898ccaffd0c83add2ff59c5327
ameneklis <ameneklis@EDPAMM>
2018-02-12 15:40:59 +0200

    Add 2 numbers initial commit

--- Solution/Solution.cs
+++ Solution/Solution.cs
@@ -8,46 +8,139 @@ namespace Solution
 {
     public class Solution
     {
-        myLinkedList l1 = new myLinkedList();
-        myLinkedList l2 = new myLinkedList();
 
-        public string GetNUmber(myLinkedList l1)
+        //public ListNode AddTwoNumbers(ListNode l1, ListNode l2)
+        //{
+        //    var n1 = GetLentgh(l1);
+        //    var n2 = GetLentgh(l2);
+        //    var n = n1 > 2 ? n1 : n2;
+        //    var res = new ListNode(0);
+        //    var tmp = new ListNode(0);
+        //    bool remain = false;
+        //    for (int i = 0; i < n; i++)
+        //    {
+        //        if (remain)
+        //        {
+        //            tmp.val = GetIvalue(l1, i) + GetIvalue(l2, i);
+        //        }
+        //        else
+        //        {
+        //            tmp.val = GetIvalue(l1, i) + GetIvalue(l2, i) + 1;
+        //        }
+        //        if (tmp.val >= 10)
+        //        {
+        //            remain = true;
+        //            tmp.val = tmp.val % 10;
+        //        }
+        //        else
+        //        {
+        //            remain = false;
+        //        }
+        //        res = tmp;
+        //        tmp.next =
+        //    }
+        //    //return numberToList(int.Parse(n1) + int.Parse(n2));
+        //    return res;
+        //}
+
+        //public int GetIvalue(ListNode l, int i)
+        //{
+        //    if (GetLentgh(l) >= i)
+        //    {
+        //        var res = 0;
+        //        var tmp = l;
+        //        for (int j = 0; j < i; j++)
+        //        {
+        //            tmp = tmp.next;
+        //        }
+        //        return tmp.val;
+        //    }
+        //    return -1;
+        //}
+
+        //public int GetLentgh(ListNode l)
+        //{
+        //    var tmp = l;
+        //    var res = 0;
+        //    while (tmp.next != null)
+        //    {
+        //        res++;
+        //        tmp = tmp.next;
+        //    }
+        //    return res + 1;
+        //}
+
+
+
+
+        public ListNode AddTwoNumbers(ListNode l1, ListNode l2)
+        {
+            var n1 = GetNUmber(l1).Reverse().ToString();
+            var n2 = GetNUmber(l2).Reverse().ToString();
+            return numberToList(double.Parse(n1) + double.Parse(n2));
+        }
+
+
+
+        public string GetNUmber(ListNode l1)
         {
             var temp = l1;
             var res = string.Empty;
-            while (!myLinkedList.IsTernary(temp))
+            while (temp.next!=null)
             {
-                var digit = temp.value.ToString();
+                var digit = temp.val.ToString();
                 res = digit + res;
-                temp = temp.node;
+                temp = temp.next;
             }
             //add last entry 
-            res = temp.value + res;
+            res = temp.val + res;
             return res;
         }
 
-        public void numberToList(int number)
+        public ListNode numberToList(double number)
         {
+            if (number.ToString().Length==1)
+            {
+                return new ListNode(int.Parse(number.ToString()));
+            }
+            var num = number.ToString().Reverse();
+            var l1 = new List<ListNode>();
+            foreach (var s in num)
+            {
+                var l = new ListNode(int.Parse(s.ToString()));               
+                l1.Add(l);
+            }
+            var res = new ListNode(l1[0].val);
+            for (int i = 0;i < l1.Count-1;i++)
+            {
+                l1[i].next = l1[i+1];               
+            }
+            res.next = l1[l1.Count - 2];
+            res.val = l1[0].val;
 
+            return res;
         }
-
 
     }
 
 
-    public class myLinkedList
+    public class ListNode
     {
-        public int value { get; set; }
-        public myLinkedList node { get; set; }
+        public int val { get; set; }
+        public ListNode next { get; set; }
 
-        public static bool IsTernary(myLinkedList l1)
+        public ListNode(int v)
         {
-            return l1.node == null;
+            this.val = v;
+        }
+        public static bool IsTernary(ListNode l1)
+        {
+            return l1.next == null;
         }
 
-        public void Connect(myLinkedList l1, myLinkedList l2)
+        public void Connect(ListNode l1, ListNode l2)
         {
-            l1.node = l2;
+            l1.next = l2;
         }
     }
 }
--- SolutionTest/TestSolutiob.cs
+++ SolutionTest/TestSolutiob.cs
@@ -11,17 +11,65 @@ namespace SolutionTest
         public void TestGetNumber1()
         {
             Solution.Solution s1 = new Solution.Solution();
-            myLinkedList l1 = new myLinkedList();
-            myLinkedList l2 = new myLinkedList();
-            myLinkedList l3 = new myLinkedList();
-            l1.value = 2;
-            l1.node = l2;
-            l2.value = 4;
-            l2.node = l3;
-            l3.value = 3;
+            ListNode l1 = new ListNode(2);
+            ListNode l2 = new ListNode(4);
+            ListNode l3 = new ListNode(3);
+
+
+
+         
             var output = s1.GetNUmber(l1);
             Assert.IsTrue(output == "342");
 
+        }
+
+
+
+        [TestMethod]
+        public void TestGetIvalue1()
+        {
+            Solution.Solution s1 = new Solution.Solution();
+            ListNode l1 = new ListNode(1);
+            ListNode l2 = new ListNode(2);
+            ListNode l3 = new ListNode(3);
+            l1.next = l2;
+            l2.next = l3;
+
+
+            ListNode l4 = new ListNode(1);
+            ListNode l5 = new ListNode(2);
+            ListNode l6 = new ListNode(3);
+            l4.next = l5;
+            l5.next = l6;
+
+
+            var output = s1.AddTwoNumbers(l1, l4);
+            Assert.IsTrue(1 == 1);
+        }
+        
+
+        [TestMethod]
+        public void TestAddTwoNumbers1()
+        {
+            Solution.Solution s1 = new Solution.Solution();
+            ListNode l1 = new ListNode(1);
+            ListNode l2 = new ListNode(0);
+
+
+            var output = s1.AddTwoNumbers(l1, l2);
+            Assert.IsTrue(output ==l1);
+        }
+
+        [TestMethod ]
+        public void TestGetLentgh1()
+        {
+            Solution.Solution s1 = new Solution.Solution();
+            ListNode l1 = new ListNode(1);
+           // ListNode l2 = new ListNode(0);
+
+
+           // var output = s1.GetLentgh(l1);
+           // Assert.IsTrue(output == 1);
         }
 
 
@@ -29,17 +77,37 @@ namespace SolutionTest
         public void TestGetNumber2()
         {
             Solution.Solution s1 = new Solution.Solution();
-            myLinkedList l1 = new myLinkedList();
-            myLinkedList l2 = new myLinkedList();
-            myLinkedList l3 = new myLinkedList();
-            l1.value = 5;
-            l1.node = l2;
-            l2.value = 6;
-            l2.node = l3;
-            l3.value = 7;
+            ListNode l1 = new ListNode(5);
+            ListNode l2 = new ListNode(6);
+            ListNode l3 = new ListNode(7);
+
+            l2.next = l3;
+            l1.next = l2;
             var output = s1.GetNUmber(l1);
             Assert.IsTrue(output == "765");
 
         }
+
+        [TestMethod]
+        public void TestGetListNode1()
+        {
+            Solution.Solution s1 = new Solution.Solution();
+           // ListNode l1 = new ListNode();
+           // ListNode l2 = new ListNode();
+          //  ListNode l3 = new ListNode();
+          
+           
+            var output = s1.numberToList(0);
+            Assert.IsTrue(1==1);
+
+        }
     }
+
+
+    //[2,4,3,2,4,3,2,4,3,2,4,3,2,4,3,2,4,3,2,4,3,2,4,3,2,4,3,2,4,3,2,4,3,2,4,3,2,4,3,2,4,3,2,4,3,2,4,3,2,4,3,2,4,3,2,4,3,2,4,3,9]
+    // [5,6,4,2,4,3,2,4,3,2,4,3,2,4,3,2,4,3,2,4,3,2,4,3,2,4,3,2,4,3,2,4,3,2,4,3,2,4,3,2,4,3,2,4,3,2,4,3,2,4,3,2,4,3,2,4,3,9,9,9,9]
+
+
+
+  
 }
